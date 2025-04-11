4cf2d08618fce26b9283bfd0bd1c57525492ec8c
Lívia Oliveira <lligiaoliveira00@gmail.com>
2025-04-11 11:34:38 -0300

    Lista – Laço de Repetição-While

--- ex3.js
+++ ex3.js
@@ -1,1 +1,11 @@
 
+let conta = 1;
+let numero = 1;
+let numerosusados = 0;
+
+console.log (' Seu resultado é :');
+while (conta <=50 ){
+    console.log (conta = conta + 3);
+    numerosusados++;
+}
+console.log (` Foram usados ${numerosusados} números`);
---
+++ ex4.js
@@ -0,0 +1,5 @@
+let numero = 1
+while (numero <=10 ){
+    console.log ( `${numero} x 5 = ${numero * 5 }`)
+    numero++
+}
